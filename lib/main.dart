a3439082bbf0cd60f6aaac32f8e94ad3476cf040
RuiXavierCosta <ruixaviercosta@gmail.com>
2019-07-09 23:03:35 +0100

    dice roll

--- lib/main.dart
+++ lib/main.dart
@@ -19,17 +19,12 @@ void main() {
   );
 }
 
+var diceValues = <int>[1, 1];
+
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: <Widget>[
-          Dice(),
-          Dice(),
-        ],
-      ),
-    );
+    return Center(child: Dice());
   }
 }
 
@@ -40,22 +35,39 @@ class Dice extends StatefulWidget {
 }
 
 class _DiceState extends State<Dice> {
-  int diceValue = 1;
+  void _rollDice() {
+    setState(() {
+      diceValues = <int>[1 + Random().nextInt(5), 1 + Random().nextInt(5)];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Expanded(
-        child: FlatButton(
-          onPressed: () {
-            setState(() {
-              diceValue = 1 + Random().nextInt(4);
-            });
-          },
-          padding: EdgeInsets.all(16.0),
-          child: Image.asset('assets/images/dice$diceValue.png'),
+    return Row(
+      children: <Widget>[
+        Container(
+          child: Expanded(
+            child: FlatButton(
+              onPressed: () {
+                _rollDice();
+              },
+              padding: EdgeInsets.all(16.0),
+              child: Image.asset('assets/images/dice${diceValues[0]}.png'),
+            ),
+          ),
         ),
-      ),
+        Container(
+          child: Expanded(
+            child: FlatButton(
+              onPressed: () {
+                _rollDice();
+              },
+              padding: EdgeInsets.all(16.0),
+              child: Image.asset('assets/images/dice${diceValues[1]}.png'),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
